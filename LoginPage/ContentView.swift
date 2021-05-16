29f82da55561a808ae6f170de671138d1f126153
Bruno Leão <leaodebrito@icloud.com>
2021-05-16 12:52:15 -0300

    Initial Commit

---
+++ LoginPage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginPage
+//
+//  Created by Bruno Brito on 16/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoginPage/LoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageApp.swift
+//  LoginPage
+//
+//  Created by Bruno Brito on 16/05/21.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
